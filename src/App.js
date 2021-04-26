087638f3d8ebf2a6d6d6d28fb2e04386d06ed23d
squeaky1273 <pjriddell22@gmail.com>
2021-04-26 13:31:16 -0700

    added list of timers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from 'react';
 import './App.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+// import new components
+import NewTimer from './components/NewTimer'
+import ListTimers from './components/ListTimers'
 
 const store = createStore(reducers);
 
 function App() {
   return (
     <Provider store={store}>
-      <h1>TMRZ</h1>
+      <div className="App">
+        <h1>TMRZ</h1>
+        {/* Display the new components */}
+        <NewTimer />
+        <ListTimers />
+      </div>
     </Provider>
   );
 }
